a8795764786166dff2faaf37e3c4f0113048d397
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-04-26 12:45:44 -0600

    Adding comments and refactoring code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,129 +1,70 @@
-// var R = require('ramda');
-// var square = function square (x) { return x * x; }
-// var squares = R.chain(square, [1, 2, 3, 4, 5]);
-//
-// document.getElementById('response').innerHTML = squares;
+const wordArray = ['nick', 'john', 'mike'];
+const word = wordArray[Math.floor((Math.random() * wordArray.length))];
 let numberOfGuesses = 0;
-
+let totalNumOfGuess = word.length;
+let key;
+let numGuess = document.getElementById('numGuess');
 let placeHolder = document.getElementById('placeHolder');
 let wrongLetterElement = document.getElementById('wrongLetter');
-let key;
-var wordArray = ['nick','john','mike'];
-var word = "nick";
-var spanElements = document.getElementsByTagName('span');
-// get the key userPressed
+let spanElements = document.getElementsByTagName('span');
 
-(function () {
+//build the place holder text _ _ _ _ _ _ _
+function buildPlaceHolder(word) {
+  Array.from(word).forEach(function(val, index, array) {
+    placeHolder.innerHTML += `<span id="${index}">_</span>`;
+  });
+};
+//get number of guesses player has. It is based on the length of word
+function getGuessNum() {
+  console.log(numGuess);
+  numGuess.innerText = `${totalNumOfGuess}`;
+}
+// listens to keyDown event. Gets Key player presses and runs dealWithKeyboard()
+(function() {
   window.addEventListener("keydown", dealWithKeyboard, false);
 })();
+
+function dealWithKeyboard(e) {
+  key = String.fromCharCode(e.keyCode).toLowerCase();
+  checkGuess(key, word);
+  winOrLose();
+  incrementNum(numberOfGuesses);
+}
+
+function checkGuess(key, word) {
+  Array.from(word).forEach(function(currentLetter, index) {
+    if (key === currentLetter) {
+      let indexOfLetter = word.indexOf(key);
+      return spanElements[indexOfLetter].innerHTML = key;
+    }
+  });
+  if (word.indexOf(key) === -1) {
+    wrongLetterElement.innerText += ` ${key} `;
+    totalNumOfGuess--;
+    getGuessNum();
+  }
+}
+
+function winOrLose() {
+  var counter = 0;
+  Array.from(spanElements).forEach(function(val) {
+    if (val.innerHTML !== '_') {
+      counter++;
+      console.log(counter);
+    }
+    if (counter === word.length + 1) {
+      document.write(`<h1> you won the game </h1>`)
+    }
+  });
+  if(totalNumOfGuess === 0) {
+    return document.write(`<h1>You lost the game </h1>`)
+  }
+}
 
 function incrementNum(num) {
   console.log('incre function')
   return num++;
 }
 
-function winOrLose(){
-  var counter = 0;
-  Array.from(spanElements).forEach(function(val) {
-    if(val.innerHTML !== '_'){
-      counter++;
-      console.log(counter)
-    }
-    if(counter === word.length){
-      document.write(`<h1> you won the game </h1>`)
-    }
-  })
-}
-
-function checkGuess(key,word) {
-  Array.from(word).forEach(function(currentLetter,index) {
-    if(key === currentLetter){
-      let indexOfLetter = word.indexOf(key);
-      return spanElements[indexOfLetter].innerHTML = key;
-    }
-  });
-  if(word.indexOf(key) === -1){
-    wrongLetterElement.innerText += ` ${key} `;
-  }
-}
-
-function dealWithKeyboard(e) {
-   key = String.fromCharCode(e.keyCode).toLowerCase();
-   checkGuess(key,word);
-   winOrLose();
-   incrementNum(numberOfGuesses);
-}
-
-// end of get key functions
-
-//  builds the placehold text _ _ _ _
-
-function buildPlaceHolder(word) {
-  Array.from(word).forEach(function(val,index,array) {
-    placeHolder.innerHTML += `<span id="${index}">_</span>`;
-  });
-};
-
-// end of placeholder
 
 buildPlaceHolder(word);
-
-
-
-
-
-
-
-
-
-
-
-// var gameLetters = document.getElementById('placeHolder');
-// var wrongLetters = document.getElementById('wrongLetter');
-// var word = Array.from("madonna");
-// var maxGuess = word.length;
-// var numberOfGuess = 0;
-// var userGuess;
-//
-// window.addEventListener("keydown", function(e) {
-//   userGuess = e.key;
-//   checkGuessAndAddLetter(userGuess,word);
-//   return userGuess;
-// });
-//
-// // function to build the _ _ _ per letter of word
-// function buildPlaceHolder(array) {
-//   word.forEach(function(letter,index,array) {
-//     gameLetters.innerHTML += `<span id="${index}">_</span>`
-//   });
-// }
-//
-// function checkGuessAndAddLetter(guess,arrayOfWord){
-//   arrayOfWord.forEach(function(letter,index,array) {
-//     if(guess === letter){
-//       var indexOfWord = document.getElementById(`${index}`);
-//       // adds letter to the placeHolder
-//       return indexOfWord.innerText = letter;
-//     }
-//     gameOver(arrayOfWord);
-//   });
-// }
-//
-// function gameOver(){
-//   var spanTags = Array.from(document.getElementsByTagName('span'));
-//   var counter = 0;
-//   spanTags.forEach(function(currentLetter) {
-//
-//    if(currentLetter.innerText == "_"){
-//     console.log(currentLetter.innerText)
-//    } else {
-//      counter++;
-//    }
-//  });
-//
-//   if(counter == word.length){
-//     document.write(`<h1>You won the game!!!!!</h1>`)
-//   }
-// }
-//
-// buildPlaceHolder(word);
